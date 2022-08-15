171378dbcca39a0f22635437b766f1f3c7d48c76
Thanusha Panaluwa <thanushapanaluwa@gmail.com>
2022-08-15 14:19:27 +0800

    Update readData.py

--- readData.py
+++ readData.py
@@ -1,5 +1,9 @@
 import csv
 
+# open CSV file and has read the data and store at the "row" variable.
+# for loop helps to read each data one by one.
+# now you can use "row" for further.
+
 with open('data.csv', 'rU')as csvfiles:
     spamreader = csv.reader(csvfiles)
     for rows in spamreader:
